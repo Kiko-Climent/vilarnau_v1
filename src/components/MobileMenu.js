6b231d206fd7b26869f7f3fe1f2ef8c6d6909010
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2024-12-11 10:10:47 +0100

    Add navbar links dinamically,add transition efx

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,6 +1,7 @@
 import { forwardRef } from "react";
 import { IoIosClose } from "react-icons/io";
 
+
 const MobileMenu = forwardRef(({ closeMenu, isVisible }, ref) => {
   return (
     <div
@@ -9,14 +10,14 @@ const MobileMenu = forwardRef(({ closeMenu, isVisible }, ref) => {
         isVisible ? "block" : "hidden"
       }`}
     >
-      <ul className="flex flex-col gap-2 w-full h-full items-center justify-center">
+      <ul className="flex flex-col gap-1 text-lg w-full h-full items-center justify-center">
         <li>prices</li>
         <li>appointments</li>
         <li>about</li>
         <li>en—de</li>
       </ul>
       <IoIosClose
-        className="absolute text-lg top-3 right-4 cursor-pointer"
+        className="absolute text-2xl top-2 right-2 cursor-pointer"
         onClick={closeMenu}
       />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Neue Montreal"', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",

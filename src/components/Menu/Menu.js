8aff36d3590898818613393e603a2f3cae9bfbec
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2025-01-01 22:36:36 +0100

    Add gsap animation to shiw hide menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,8 +1,11 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './menu.css';
 import Link from 'next/link';
+
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 const menuLinks = [
   {path: '/', label: 'Home'},
@@ -15,9 +18,38 @@ const Menu = () => {
   const container = useRef()
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
+  const tl = useRef();
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  useGSAP(() => {
+    gsap.set('.menu-link-item-holder', {y: 75});
+
+    tl.current = gsap.timeline({ paused: true})
+    .to('.menu-overlay', {
+      duration: 1.25,
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      ease: 'power4.inOut',
+    })
+    .to('.menu-link-item-holder', {
+      y: 0,
+      duration: 0.1,
+      stagger: 0.1,
+      ease: 'power4.inOut',
+      delay: -0.75,
+    })
+  }, 
+  { scope: container });
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      tl.current.play();
+    } else {
+      tl.current.reverse();
+    }
+  }, [isMenuOpen])
 
   return(
     <div className="menu-container" ref={container}>

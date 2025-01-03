519b448fa4e36f46d20a6c1219b0ee98c503eab3
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2025-01-03 18:47:52 +0100

    Add more animation to Hero section

--- src/components/Hero/animations.js
+++ src/components/Hero/animations.js
@@ -3,7 +3,6 @@ import { gsap } from "gsap";
 export const revealHero = (progressRef) => {
   const tl = gsap.timeline();
 
-  // Asegura que la barra comienza visible
   tl.fromTo(
     progressRef.current,
     { scaleY: 1 },
@@ -17,3 +16,43 @@ export const revealHero = (progressRef) => {
 
   return tl;
 };
+
+export const blurHero = (imageRef) => {
+  const tl = gsap.timeline();
+
+  tl.to(imageRef.current, {
+    filter: 'blur(10px)',
+    duration: 0,
+    ease: 'power3.inOut',
+  })
+  .to(imageRef.current, {
+    filter: 'blur(0px)',
+    duration: 1.5,
+    delay: 1,
+    // ease: 'power2.out'
+    ease: 'expo.out'
+  });
+
+  return tl;
+};
+
+export const revealTextHero = (textRef) => {
+  const tl = gsap.timeline();
+
+  const spans = textRef.current.querySelectorAll("span");
+
+  tl.fromTo(
+    spans,
+    { y: 20, opacity: 0, rotationX: 15 },
+    {
+      y: 0,
+      opacity: 2,
+      rotationX: 0,
+      duration: 1,
+      ease: "power2.out",
+      stagger: 0.15,
+    }
+  );
+
+  return tl;
+}
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,11 +3,13 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
-import { revealHero } from "./animations";
+import { revealHero, blurHero, revealTextHero } from "./animations";
 
 const Hero = () => {
   const progressRef = useRef(null);
   const heroRef = useRef(null);
+  const imageRef = useRef(null);
+  const textRef = useRef(null);
 
   useEffect(() => {
     const tl = gsap.timeline();
@@ -15,6 +17,8 @@ const Hero = () => {
     
     if (progressRef.current) {
       tl.add(revealHero(progressRef));
+      tl.add(blurHero(imageRef), "-=1");
+      tl.add(revealTextHero(textRef), "-=2");
     } else {
       console.error("progressRef no está asignado correctamente.");
     }
@@ -38,11 +42,18 @@ const Hero = () => {
           src="/images/Vilarnau x Palma Llopis-18.jpg"
           alt="Hero"
           className="object-cover w-full h-full"
+          ref={imageRef}
         />
       </div>
 
-      <div className="absolute inset-0 flex items-center justify-center text-white uppercase">
-        <h1 className="text-7xl font-bold">Vilarnau.</h1>
+      <div ref={textRef} className="absolute inset-0 flex items-center justify-center text-white uppercase">
+        <h1 className="text-6xl md:text-7xl font-bold">
+          {"VILARNAU".split("").map((letter, index) => (
+            <span key={index} className="inline-block">
+              {letter}
+            </span>
+          ))}
+        </h1>
       </div>
     </section>
   );
